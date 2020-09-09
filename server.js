d95e3ed8877d610f9d8defca610341f91b771be9
Usama Qureshi <usamaqureshi06@users.noreply.github.com>
2020-09-09 13:55:17 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app=express();
+
+const PORT = process.env.PORT||5000;
+
+app.get('/',(req,res)=>res.send('Api Running'));
+
+app.listen(PORT,()=>{console.log(`Server started on Port ${PORT}`)});
