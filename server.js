fc636465378b4a2d816abe03d82dd3e001062cd0
Usama Qureshi <usamaqureshi06@users.noreply.github.com>
2020-10-06 16:46:21 -0400

    Developers Social Platform
    Social media for developers.

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app=express();
+//connect Database
+connectDB();
+
+//init Middelware
+app.use(express.json({extened:false}));
 
 const PORT = process.env.PORT||5000;
 
 app.get('/',(req,res)=>res.send('Api Running'));
+
+
+// Define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
 
 app.listen(PORT,()=>{console.log(`Server started on Port ${PORT}`)});
