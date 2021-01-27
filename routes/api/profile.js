4a596600d174a9e589c533d773da6bac1fef9db5
Usama Qureshi <usamaqureshi06@users.noreply.github.com>
2021-01-27 04:20:23 -0500

    Developers Connect
    Social platform for Developers to connect

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -14,8 +14,10 @@ const { response } = require('express');
 
 router.get('/me',auth, async (req,res)=>{
     try{
-        const profile = await Profile.findOne({user: req.user.id}).populate('user',
-        ['name','avatar']);
+        const profile = await Profile.findOne({user: req.user.id}).populate(
+            'user',
+            ['name','avatar']
+        );
         if(!profile){
             return res.status(400).json({msg:'There is no profile for this user'});
         }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app=express();
 //connect Database
@@ -10,7 +11,7 @@ app.use(express.json({extened:false}));
 
 const PORT = process.env.PORT||5000;
 
-app.get('/',(req,res)=>res.send('Api Running'));
+//app.get('/',(req,res)=>res.send('Api Running'));
 
 
 // Define routes
@@ -19,6 +20,17 @@ app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
 
+//Serve static assets in production
+
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) =>{
+
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 
 app.listen(PORT,()=>{console.log(`Server started on Port ${PORT}`)});
